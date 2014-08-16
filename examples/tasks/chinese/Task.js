c4e8296fdfb073e6099c1c153d08861321c9c6d8
Howard.Zuo <leftstick@qq.com>
2014-08-16 12:23:43 +0800

    import new version of terminal-men which fixed the chinese character issue

--- examples/tasks/chinese/Task.js
+++ examples/tasks/chinese/Task.js
@@ -5,7 +5,7 @@ var logger = TaskRunner.logger;
 
 var Task = TaskRunner.Base.extend({
     id: 'chinese',
-    name: '这是一个中文task',
+    name: '这是一个中文任务,样子如何？是否很high',
     position: 5,
     run: function(cons) {
         logger.warn('您好，世界');
